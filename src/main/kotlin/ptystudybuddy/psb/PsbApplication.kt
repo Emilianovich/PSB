dd01e5ec6ac5852e941185f1b7033238e947f4ae
Emilianovich <emiolafagbemi02@gmail.com>
2026-03-09 01:02:30 -0500

    refactor: fix formatting styles

--- src/main/kotlin/ptystudybuddy/psb/PsbApplication.kt
+++ src/main/kotlin/ptystudybuddy/psb/PsbApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class PsbApplication
 
 fun main(args: Array<String>) {
-	runApplication<PsbApplication>(*args)
+    runApplication<PsbApplication>(*args)
 }
