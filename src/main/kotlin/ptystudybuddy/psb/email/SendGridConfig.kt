e571ad5cc46954da7e487a37c7e9a9338fa94335
Emilianovich <emiolafagbemi02@gmail.com>
2026-03-09 23:31:59 -0500

    feat: completed implementing SendGrid config, service and Date and Time format helper

---
+++ src/main/kotlin/ptystudybuddy/psb/email/SendGridConfig.kt
@@ -0,0 +1,15 @@
+package ptystudybuddy.psb.email
+
+import com.sendgrid.SendGrid
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SendGridConfig(
+    @Value("\${sendgrid.key}")
+    private val key: String,
+) {
+    @Bean
+    fun sendGridClient(): SendGrid = SendGrid(key)
+}
