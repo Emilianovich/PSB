7f598c667882499c1f6cb21a0b46f5a4d53b03e2
Emilianovich <emiolafagbemi02@gmail.com>
2026-03-10 00:47:38 -0500

    fix: added sourcery suggestions

--- src/main/kotlin/ptystudybuddy/psb/exceptions/GlobalExceptionHandler.kt
+++ src/main/kotlin/ptystudybuddy/psb/exceptions/GlobalExceptionHandler.kt
@@ -12,23 +12,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 import ptystudybuddy.psb.exceptions.customs.UnprocessableEntityException
 import ptystudybuddy.psb.presentation.ExceptionRes
 
-
 @RestControllerAdvice
 class GlobalExceptionHandler {
     // Status Code 400 when DTO validation fails
     @ExceptionHandler(MethodArgumentNotValidException::class)
-    fun handleBadRequest(e: MethodArgumentNotValidException): ResponseEntity<ExceptionRes<List<Map<String, String?>>>?>? {
-        val errors = e.bindingResult.fieldErrors.map {
+    fun handleBadRequest(e: MethodArgumentNotValidException): ResponseEntity<ExceptionRes<List<Map<String, String?>>>> {
+        val errors =
+            e.bindingResult.fieldErrors.map {
+                mapOf(
+                    "field" to it.field,
+                    "message" to it.defaultMessage,
+                )
+            }
+        val body =
             mapOf(
-                "field" to it.field,
-                "message" to it.defaultMessage
+                "success" to false,
+                "statusCode" to HttpStatus.BAD_REQUEST.value(),
+                "content" to errors,
             )
-        }
-        val body = mapOf(
-            "success" to false,
-            "statusCode" to HttpStatus.BAD_REQUEST.value(),
-            "content" to errors
-        )
         return ResponseEntity.badRequest().body(ExceptionRes(statusCode = e.statusCode.value(), errors = errors))
     }
 
@@ -41,26 +42,27 @@ class GlobalExceptionHandler {
         UnprocessableEntityException::class,
     )
     fun handleAccessDeniedException(ex: Exception): ResponseEntity<ExceptionRes<String>> {
-        val status = when (ex) {
-            is HttpMessageNotReadableException -> HttpStatus.BAD_REQUEST
-            is AccessDeniedException -> HttpStatus.FORBIDDEN
-            is EntityNotFoundException -> HttpStatus.NOT_FOUND
-            is DataIntegrityViolationException -> HttpStatus.CONFLICT
-            is UnprocessableEntityException -> HttpStatus.UNPROCESSABLE_ENTITY
-            else -> HttpStatus.BAD_REQUEST
-        }
+        val status =
+            when (ex) {
+                is HttpMessageNotReadableException -> HttpStatus.BAD_REQUEST
+                is AccessDeniedException -> HttpStatus.FORBIDDEN
+                is EntityNotFoundException -> HttpStatus.NOT_FOUND
+                is DataIntegrityViolationException -> HttpStatus.CONFLICT
+                is UnprocessableEntityException -> HttpStatus.UNPROCESSABLE_ENTITY
+                else -> HttpStatus.BAD_REQUEST
+            }
         return ResponseEntity.status(status.value())
             .body(ExceptionRes(statusCode = status.value(), errors = ex.message ?: "Un error del tipo ${status.name} ocurrió"))
     }
 
     // Status Code 415
     @ExceptionHandler(HttpMediaTypeNotSupportedException::class)
-    fun handleUnsupportedException(ex: Exception): ResponseEntity<ExceptionRes<String>> {
+    fun handleUnsupportedException(ex: HttpMediaTypeNotSupportedException): ResponseEntity<ExceptionRes<String>> {
         return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()).body(
             ExceptionRes(
                 statusCode = HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(),
-                errors = ex.message ?: "Un error del tipo ${ex.cause} ocurrió"
-            )
+                errors = ex.message ?: "Un error del tipo ${ex.cause} ocurrió",
+            ),
         )
     }
 
@@ -70,8 +72,8 @@ class GlobalExceptionHandler {
         return ResponseEntity.internalServerError().body(
             ExceptionRes(
                 statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                errors = e.message ?: "Error interno del servidor"
-            )
+                errors = e.message ?: "Error interno del servidor",
+            ),
         )
     }
 }
