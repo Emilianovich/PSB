f1765ea2e458c141ccc840d25dcff349e0400483
Emilianovich <emiolafagbemi02@gmail.com>
2026-03-10 22:00:32 -0500

    fix: allegedly added formatting

--- src/main/kotlin/ptystudybuddy/psb/bucket/BucketService.kt
+++ src/main/kotlin/ptystudybuddy/psb/bucket/BucketService.kt
@@ -1,5 +1,7 @@
 package ptystudybuddy.psb.bucket
 
+import java.time.Duration
+import java.util.UUID
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -11,123 +13,86 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.model.S3Exception
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
-import java.time.Duration
-import java.util.UUID
 
 @Service
-class BucketService(
-    private val s3Client: S3Client,
-    private val s3Presigner: S3Presigner,
-) {
-    @Value(value = "\${bucket.name}")
-    lateinit var bucketName: String
+class BucketService(private val s3Client: S3Client, private val s3Presigner: S3Presigner) {
+  @Value(value = "\${bucket.name}") lateinit var bucketName: String
 
-    val waiterDuration = 30L
-    val signedUrlDuration = 3L
+  val waiterDuration = 30L
+  val signedUrlDuration = 3L
 
-    fun upload(file: MultipartFile): String {
-        try {
-            val extension =
-                file.originalFilename
-                    ?.substringAfterLast('.', "")
-                    ?.takeIf { it.isNotBlank() }
+  fun upload(file: MultipartFile): String {
+    try {
+      val extension = file.originalFilename?.substringAfterLast('.', "")?.takeIf { it.isNotBlank() }
 
-            val newKey =
-                if (extension != null) {
-                    "${UUID.randomUUID()}.$extension"
-                } else {
-                    UUID.randomUUID().toString()
-                }
-
-            val fileUploadRequest =
-                PutObjectRequest
-                    .builder()
-                    .bucket(bucketName)
-                    .key(newKey)
-                    .contentType(file.contentType)
-                    .build()
-
-            s3Client.putObject(
-                fileUploadRequest,
-                RequestBody.fromBytes(file.bytes),
-            )
-
-            this.s3Client.waiter().waitUntilObjectExists(
-                {
-                    it.bucket(bucketName)
-                        .key(newKey)
-                },
-                { it.waitTimeout(Duration.ofSeconds(waiterDuration)) },
-            )
-
-            return newKey
-        } catch (e: S3Exception) {
-            throw IllegalArgumentException(
-                "File Deletion Failed: ${e.message}",
-                e,
-            )
+      val newKey =
+        if (extension != null) {
+          "${UUID.randomUUID()}.$extension"
+        } else {
+          UUID.randomUUID().toString()
         }
+
+      val fileUploadRequest =
+        PutObjectRequest.builder()
+          .bucket(bucketName)
+          .key(newKey)
+          .contentType(file.contentType)
+          .build()
+
+      s3Client.putObject(fileUploadRequest, RequestBody.fromBytes(file.bytes))
+
+      this.s3Client
+        .waiter()
+        .waitUntilObjectExists(
+          { it.bucket(bucketName).key(newKey) },
+          { it.waitTimeout(Duration.ofSeconds(waiterDuration)) },
+        )
+
+      return newKey
+    } catch (e: S3Exception) {
+      throw IllegalArgumentException("File Deletion Failed: ${e.message}", e)
     }
+  }
 
-    fun delete(fileKey: String): Boolean {
-        try {
-            val deleteObjectRequest =
-                DeleteObjectRequest
-                    .builder()
-                    .bucket(bucketName)
-                    .key(fileKey)
-                    .build()
+  fun delete(fileKey: String): Boolean {
+    try {
+      val deleteObjectRequest =
+        DeleteObjectRequest.builder().bucket(bucketName).key(fileKey).build()
 
-            this.s3Client.deleteObject(deleteObjectRequest)
-            this.s3Client.waiter().waitUntilObjectNotExists(
-                {
-                    it.bucket(bucketName)
-                        .key(fileKey)
-                },
-                { it.waitTimeout(Duration.ofSeconds(waiterDuration)) },
-            )
+      this.s3Client.deleteObject(deleteObjectRequest)
+      this.s3Client
+        .waiter()
+        .waitUntilObjectNotExists(
+          { it.bucket(bucketName).key(fileKey) },
+          { it.waitTimeout(Duration.ofSeconds(waiterDuration)) },
+        )
 
-            return true
-        } catch (e: S3Exception) {
-            throw IllegalArgumentException(
-                "File Deletion Failed: ${e.message}",
-                e,
-            )
-        }
+      return true
+    } catch (e: S3Exception) {
+      throw IllegalArgumentException("File Deletion Failed: ${e.message}", e)
     }
+  }
 
-    fun update(
-        file: MultipartFile,
-        fileKey: String,
-    ): String {
-        this.delete(fileKey)
-        return this.upload(file)
+  fun update(file: MultipartFile, fileKey: String): String {
+    this.delete(fileKey)
+    return this.upload(file)
+  }
+
+  fun getSignedUrl(fileKey: String): String {
+    try {
+      val fileUpload = GetObjectRequest.builder().bucket(bucketName).key(fileKey).build()
+
+      val urlSigned =
+        GetObjectPresignRequest.builder()
+          .signatureDuration(Duration.ofHours(signedUrlDuration))
+          .getObjectRequest(fileUpload)
+          .build()
+
+      val presignedObject = s3Presigner.presignGetObject(urlSigned)
+
+      return presignedObject.url().toString()
+    } catch (e: S3Exception) {
+      throw IllegalArgumentException("File Deletion Failed: ${e.message}", e)
     }
-
-    fun getSignedUrl(fileKey: String): String {
-        try {
-            val fileUpload =
-                GetObjectRequest
-                    .builder()
-                    .bucket(bucketName)
-                    .key(fileKey)
-                    .build()
-
-            val urlSigned =
-                GetObjectPresignRequest
-                    .builder()
-                    .signatureDuration(Duration.ofHours(signedUrlDuration))
-                    .getObjectRequest(fileUpload)
-                    .build()
-
-            val presignedObject = s3Presigner.presignGetObject(urlSigned)
-
-            return presignedObject.url().toString()
-        } catch (e: S3Exception) {
-            throw IllegalArgumentException(
-                "File Deletion Failed: ${e.message}",
-                e,
-            )
-        }
-    }
+  }
 }
