5a02c7cd0cdeb74c0a5c910220fc59d6fedf35d0
Emilianovich <emiolafagbemi02@gmail.com>
2026-03-10 22:00:32 -0500

    fix: allegedly added formatting

--- src/main/kotlin/ptystudybuddy/psb/bucket/BucketConfiguration.kt
+++ src/main/kotlin/ptystudybuddy/psb/bucket/BucketConfiguration.kt
@@ -9,22 +9,15 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner
 
 @Configuration
 class BucketConfiguration {
-    @Value("\${bucket.region}")
-    lateinit var bucketRegion: String
+  @Value("\${bucket.region}") lateinit var bucketRegion: String
 
-    @Bean
-    fun s3Client(): S3Client {
-        return S3Client
-            .builder()
-            .region(Region.of(bucketRegion))
-            .build()
-    }
+  @Bean
+  fun s3Client(): S3Client {
+    return S3Client.builder().region(Region.of(bucketRegion)).build()
+  }
 
-    @Bean
-    fun s3Presigner(): S3Presigner {
-        return S3Presigner
-            .builder()
-            .region(Region.of(bucketRegion))
-            .build()
-    }
+  @Bean
+  fun s3Presigner(): S3Presigner {
+    return S3Presigner.builder().region(Region.of(bucketRegion)).build()
+  }
 }
