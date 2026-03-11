1774e4697d06158495b6b1c0a6aeea03308a8594
Steven Ampie <ampiesteven24@gmail.com>
2026-03-11 02:02:59 -0500

    feat: added JsonIgnore to OneToMany entities

--- src/main/kotlin/ptystudybuddy/psb/entities/TutorsEntity.kt
+++ src/main/kotlin/ptystudybuddy/psb/entities/TutorsEntity.kt
@@ -13,17 +13,19 @@ import java.math.BigDecimal
 @Entity
 @Table(name = "tutors")
 class TutorsEntity(
-    @Id @GeneratedValue(strategy = GenerationType.UUID) val id: String? = null,
-    @Column(name = "social_id", nullable = false) val socialId: String,
-    @Column(nullable = false) val fullname: String,
-    @Column(nullable = false) val cv: String,
-    @Column(nullable = false, unique = true) val email: String,
-    @Column(nullable = false) val password: String,
-    @Column val picture: String,
-    @Column(precision = 3, scale = 2) val score: BigDecimal,
-    val role: String = "TUTOR",
-    @OneToMany(mappedBy = "tutor_id")   @JsonIgnore val reviews: MutableList<ReviewsEntity> = mutableListOf(),
-    @OneToMany(mappedBy = "tutor_id")
-    @JsonIgnore
+  @Id @GeneratedValue(strategy = GenerationType.UUID) val id: String? = null,
+  @Column(name = "social_id", nullable = false) val socialId: String,
+  @Column(nullable = false) val fullname: String,
+  @Column(nullable = false) val cv: String,
+  @Column(nullable = false, unique = true) val email: String,
+  @Column(nullable = false) val password: String,
+  @Column val picture: String,
+  @Column(precision = 3, scale = 2) val score: BigDecimal,
+  val role: String = "TUTOR",
+  @OneToMany(mappedBy = "tutor_id")
+  @JsonIgnore
+  val reviews: MutableList<ReviewsEntity> = mutableListOf(),
+  @OneToMany(mappedBy = "tutor_id")
+  @JsonIgnore
   val sessions_assignments: MutableList<SessionAssignmentEntity> = mutableListOf(),
 )
