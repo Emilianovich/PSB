28d063ed2f42ca0d6e4e60553f1fdd90b0fd1bad
Steven Ampie <ampiesteven24@gmail.com>
2026-03-14 23:46:23 -0500

    style: removed unnecessary code in TutorsSpecification

--- src/main/kotlin/ptystudybuddy/psb/tutors/TutorsSpecification.kt
+++ src/main/kotlin/ptystudybuddy/psb/tutors/TutorsSpecification.kt
@@ -1,11 +1,7 @@
 package ptystudybuddy.psb.tutors
 
-import jakarta.persistence.criteria.JoinType
 import jakarta.persistence.criteria.Predicate
 import org.springframework.data.jpa.domain.Specification
-import ptystudybuddy.psb.entities.InscriptionsEntity
-import ptystudybuddy.psb.entities.SessionAssignmentEntity
-import ptystudybuddy.psb.entities.SessionsEntity
 import ptystudybuddy.psb.entities.TutorsEntity
 
 object TutorsSpecification {
@@ -13,24 +9,10 @@ object TutorsSpecification {
   fun fullNameFilter(fullname: String?, orderBy: String): Specification<TutorsEntity> {
 
     return Specification { root, query, cb ->
-      val assignments =
-        root.join<TutorsEntity, SessionAssignmentEntity>("sessionsAssignment", JoinType.INNER)
-
-      val sessions =
-        assignments.join<SessionAssignmentEntity, SessionsEntity>("sessionId", JoinType.INNER)
-
-      val inscriptions =
-        sessions.join<InscriptionsEntity, SessionsEntity>("inscriptions", JoinType.LEFT)
-
       val filters = mutableListOf<Predicate>()
 
       fullname?.let { filters.add(cb.like(root.get("fullname"), "%$it%")) }
 
-      filters.add(
-        cb.notEqual(sessions.get<Int>("availableSlots"), sessions.get<Int>("expectedStudents"))
-      )
-
-      query?.having(cb.notEqual(cb.count(inscriptions.get<String>("sessionId")), 0))
       query?.groupBy(root.get<String>("id"))
 
       val order =
