a8d180afffd6aff9cee47a6f0d7bcb02cc7c2601
Emilianovich <emiolafagbemi02@gmail.com>
2026-03-16 10:35:23 -0500

    feat: added schedules and inscriptions list endpoints

--- src/main/kotlin/ptystudybuddy/psb/inscriptions/InscriptionController.kt
+++ src/main/kotlin/ptystudybuddy/psb/inscriptions/InscriptionController.kt
@@ -3,6 +3,7 @@ package ptystudybuddy.psb.inscriptions
 import jakarta.validation.Valid
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/inscriptions")
 class InscriptionController(private val inscriptionService: InscriptionService) {
+  @GetMapping("/{sessionId}")
+  @PreAuthorize("hasRole('ADMIN') or hasRole('TUTOR')")
+  fun getAll(@PathVariable sessionId: String) =
+    inscriptionService.getAllInscriptionsPerSession(sessionId)
+
   @PostMapping
   @PreAuthorize("hasRole('STUDENT')")
   fun create(@Valid @RequestBody req: CreateInscriptionReq) = inscriptionService.create(req)
