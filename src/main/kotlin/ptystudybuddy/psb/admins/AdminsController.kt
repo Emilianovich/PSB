6e9c59368cb1347de8e07bb3ac35f60c46680515
Steven Ampie <ampiesteven24@gmail.com>
2026-03-16 00:14:02 -0500

    style: added format

--- src/main/kotlin/ptystudybuddy/psb/admins/AdminsController.kt
+++ src/main/kotlin/ptystudybuddy/psb/admins/AdminsController.kt
@@ -5,15 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/admins")
-class AdminsController(
-    private val adminsService: AdminsService
-){
+class AdminsController(private val adminsService: AdminsService) {
 
-    @GetMapping("/profile")
-    fun getProfile() = adminsService.getProfile()
-
+  @GetMapping("/profile") fun getProfile() = adminsService.getProfile()
 }
